87fadb0ca0826e882222334b0ed73b9745cce2aa
dy/dx <77755728+dy-dx-1@users.noreply.github.com>
2023-02-13 14:50:52 -0500

    Template pour preuve de concept

---
+++ main/deplacements.h
@@ -0,0 +1,12 @@
+/// Fonctions associées au déplacement du robot 
+
+void move_front(int x){
+    // Fonction permettant de se déplacer d'une distance de x cm 
+    // je te laisse libre si tu veux faire plusieurs fonctions (ex move_front, move_back, turn_right, turn_left, etc)
+    // ou si tu veux handle tout avec une fonction et des paramètres (ex: move(distance, front) )
+    // perso je préfère cette dernière option, c'est plus clair 
+}
+
+void turn(int theta){
+    // Fonction permettant de tourner sur place
+}
--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,10 @@
-void setup() {
+/// Code central qui gère toute la logique des opérations
 
+// Importation des modules 
+#include "deplacements.h"
+
+void setup() {
+    
 }
 
 void loop() {
