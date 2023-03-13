3597894dfafd6c6087d44d8a11d59e4bde68b0e7
dy/dx <77755728+dy-dx-1@users.noreply.github.com>
2023-03-13 12:23:50 -0400

    base add lift_box open_box

--- main/operation_pince.h
+++ main/operation_pince.h
@@ -1,13 +1,24 @@
 ////// Fonctions associées à l'opération de la pince et de son système d'élévation 
  
-int convert_width_steps(int width){
-  //// Prend une largeur en mm et la converti en le nb de pas que le pignon doit faire pour ouvrir la pince 
+void lift_box(int height){
+  //// Prend une hautur en mm et la converti en le nb de pas que le stepper doit faire pour monter/descendre la plaque
   int steps{}; 
-  // wow des calculs 
-  return steps; 
+  // conversion hauteur-steps 
+  // Translation 
+  Serial.print("Translation de la boîte de ");Serial.print(height); Serial.println(" mm"); 
+  digitalWrite(led_bleue, HIGH); 
+  moteur_lift.move(steps); 
+  while (moteur_lift.distanceToGo()!=0){
+    // attendre tant qu'on est pas rendus 
+    moteur_lift.run(); 
+  }
+  Serial.println("Translation de la boîte finie"); 
+  digitalWrite(led_bleue, LOW); 
 }
 
 void open_box(int width){
   //// Ouvre la pince d'un certain nb de mm 
-  Serial.println("Ouverture de la pince"); 
+  // On considère qu'il n'y a pas de glissement entre la crémaillère et le pignon 
+  int angle = (width / (diametre_pignon/2))*(180/3.14159); // angle en radians converti en degrés  
+  Serial.print("Ouverture de la pince commencée: "); Serial.print(angle); Serial.println("degrés"); 
 }
