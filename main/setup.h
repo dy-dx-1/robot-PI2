3b5c10e00fea6f347f1400994138c03e32bd3b10
dy/dx <77755728+dy-dx-1@users.noreply.github.com>
2023-03-13 12:23:18 -0400

    setup blue led

--- main/setup.h
+++ main/setup.h
@@ -10,6 +10,8 @@ const float stepper_accel = 400;    // steps/s^2
 const int rayon_roue = 33;          // rayon des roues motrices en mm 
 const int d_entre_roues = 132;      // distance entre les roues motrices en mm 
 
+const int diametre_pignon = 24;     // Diamètre du pignon du servo de la boîte en mm 
+
 // Création des objets représentant les moteurs 
 AccelStepper moteur_G(AccelStepper::FULL4WIRE, 11, 9, 10, 8);
 AccelStepper moteur_D(AccelStepper::FULL4WIRE, 7, 5, 6, 4);
@@ -20,7 +22,8 @@ Servo pignon;
 const int button = 53;          // bouton de démarrage 
 const int led_rouge = 46;   
 const int led_jaune = 48;   
-const int led_verte = 50;   
+const int led_verte = 50;  
+const int led_bleue = 52;  
 const int servo_pin = 12; 
 
 void setup() {
@@ -29,6 +32,7 @@ void setup() {
   pinMode(led_rouge, OUTPUT);
   pinMode(led_jaune, OUTPUT);
   pinMode(led_verte, OUTPUT);
+  pinMode(led_bleue, OUTPUT); 
   pinMode(button, INPUT_PULLUP);  // utilisation d'un pullup resistor pour façiliter l'utilisation du bouton
 
   void stepper_setup(); 
